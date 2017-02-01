b2b4626d03d1b109307b896aca208bf48c4a4a72
DinoTrainer <tunell.john@gmail.com>
2017-02-01 11:22:55 +0100

    Getting started with the book

---
+++ chapter_1/first_again.py
@@ -0,0 +1,50 @@
+from collections import Counter
+
+users = [
+    { "id": 0, "name": "Hero" },
+    { "id": 1, "name": "Dunn" },
+    { "id": 2, "name": "Sue" },
+    { "id": 3, "name": "Chi" },
+    { "id": 4, "name": "Thor" },
+    { "id": 5, "name": "Clive" },
+    { "id": 6, "name": "Hicks" },
+    { "id": 7, "name": "Devin" },
+    { "id": 8, "name": "Kate" },
+    { "id": 9, "name": "Klein" }
+]
+
+friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+               (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
+
+for user in users:
+    user['friends'] = []
+
+
+for i, j in friendships:
+    users[i]['friends'].append(users[j])
+    users[j]['friends'].append(users[i])
+
+def number_of_friends(user):
+    return len(user['friends'])
+
+total_connections = sum(number_of_friends(user)
+                        for user in users)
+
+num_of_friends_by_id = [(user['id'], number_of_friends(user))
+                           for user in users]
+
+print(number_of_friends(users[9]))
+
+print(num_of_friends_by_id)
+
+#num_friends_by_id = [(user['id'], number_of_friends(user))
+                     #for user in users]
+
+
+#def friends_of_friend_ids_bad(user):
+
+
+
+#print(friends_of_friend_ids_bad(users[1]))
+
